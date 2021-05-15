3b5750f5def446e3d419bdf3a94244a3ba7e837d
JavaScriptReact <nevim@seznam.cz>
2021-05-15 19:23:21 +0200

    commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = requrie("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server is listening on PORT ", PORT));
